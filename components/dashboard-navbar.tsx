5a8d6cd78e64a309c3e13b574dc3a5eaab42e0c1
yahyashhah <yahyashah.dev@gmail.com>
2025-05-09 16:21:23 +0500

    feat: add organization management routes and update dashboard navbar with organization switcher

--- components/dashboard-navbar.tsx
+++ components/dashboard-navbar.tsx
@@ -2,21 +2,37 @@
 
 import { getApiLimit } from "@/lib/api-limits";
 import { checkSubscription } from "@/lib/subscription";
-import { UserButton } from "@clerk/nextjs";
+import { UserButton, SignedIn, OrganizationSwitcher } from "@clerk/nextjs";
 import MobileSidebar from "./mobile-sidebar";
 
-// import { UserButton } from "@clerk/nextjs";
-
 const Navbar = async () => {
-  const apiLimitCount = await getApiLimit() as number;
-  const isPro = await checkSubscription() as boolean;
+  const apiLimitCount = (await getApiLimit()) as number;
+  const isPro = (await checkSubscription()) as boolean;
+
   return (
-    <div className="flex bg-[#161717] items-center p-6">
-      <MobileSidebar isPro={isPro} apiLimitCount={apiLimitCount} />
-      <div className="flex w-full justify-end">
-        <UserButton />
+    <div className="flex bg-[#161717] items-center justify-between p-6">
+      <div className="flex items-center gap-4">
+        <MobileSidebar isPro={isPro} apiLimitCount={apiLimitCount} />
+        
+       
+        {/* Show Organization Switcher when signed in */}
+        <SignedIn>
+          <OrganizationSwitcher
+            appearance={{
+              elements: {
+                organizationSwitcherTrigger: "text-white",
+              },
+            }}
+          />
+        </SignedIn>
       </div>
+
+      {/* User profile menu */}
+      <SignedIn>
+        <UserButton afterSignOutUrl="/" />
+      </SignedIn>
     </div>
   );
 };
+
 export default Navbar;
